7f4cbbb0099cab4c245fc02a15ef56a8168343e4
irsad-najib <156823001+irsad-najib@users.noreply.github.com>
2024-08-28 22:16:03 +0700

    perubahan untuk app

---
+++ packages/shared/btn.js
@@ -0,0 +1,13 @@
+import react from "react";
+import { useState } from react;
+
+export default function Button() {
+    const handleButtonClick = () => {
+        setSelectedButton();
+    };
+
+    const handleThemeClick = () => {
+        setSelectedTheme();
+    };
+
+}
